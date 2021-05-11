f2c7cc2a1322dcac6bb1cda8e093b69cd016b8b4
Josh Burns <joshyburnss@gmail.com>
2021-05-11 19:33:47 +1000

    autostart statusbar script from xinit

--- home/.bashrc
+++ home/.bashrc
@@ -7,10 +7,12 @@ alias ga="git add"
 alias gcmsg="git commit -m"
 alias gc="git commit"
 alias gc!="git commit --amend"
+alias gst="git status"
 alias src="clear && source ~/.bashrc"
 
 # Path
 export PATH="$HOME/.emacs.d/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 # ASDF
 . $HOME/.asdf/asdf.sh
--- home/.doom.d/custom.el
+++ home/.doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
  '(package-selected-packages '(helm-org-rifle)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
