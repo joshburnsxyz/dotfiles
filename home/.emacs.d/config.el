270afa4f6ba19a1fffefeded0e4d80de3d7cf53e
Josh Burns <joshyburnss@gmail.com>
2021-05-29 15:46:31 +1000

    ignore config.el

--- home/.emacs.d/config.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(add-hook 'text-mode-hook 'display-line-numbers-mode)
-
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq x-select-enable-clipboard t)
